5f4eac8f25773bdfda9d9e13a4d4ec20c06bb4f1
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2023-08-25 13:20:47 +0200

    First version of main with all arguments hard-coded

--- src/main.rs
+++ src/main.rs
@@ -6,44 +6,46 @@ mod test_data;
 mod tokenizer;
 
 use anyhow::{Ok, Result};
-use byteorder::{LittleEndian, ReadBytesExt};
-use std::fs::File;
-use std::io::prelude::*;
 
-use model::Embedding;
+use ndarray::{Array, Axis};
+use rand::{rngs::StdRng, SeedableRng};
+
+use model::Transformer;
+use tokenizer::Tokenizer;
 
 fn main() -> Result<()> {
-    // Open the file
-    let mut f = File::open("stories15M.bin")?;
-    let mut buffer = [0; 28]; // 7 integers * 4 bytes each
+    let path = "stories15M.bin";
+    let start = "One day, Lily met a Shoggoth";
+    let num_samples = 2;
+    let max_new_tokens = 100; // number of tokens generated in each sample
+    let temperature = 1.0; // 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
+    let top_k = 300; // retain only the top_k most likely tokens, clamp others to have 0 probability
 
-    // Read the first 28 bytes (header)
-    f.read_exact(&mut buffer)?;
+    let seed = [13; 32];
+    let mut rng = StdRng::from_seed(seed);
 
-    // Convert bytes to integers
-    let mut cursor = std::io::Cursor::new(buffer);
-    let dim = cursor.read_i32::<LittleEndian>()?;
-    let hidden_dim = cursor.read_i32::<LittleEndian>()?;
-    let n_layers = cursor.read_i32::<LittleEndian>()?;
-    let n_heads = cursor.read_i32::<LittleEndian>()?;
-    let n_kv_heads = cursor.read_i32::<LittleEndian>()?;
-    let vocab_size = cursor.read_i32::<LittleEndian>()?;
-    let max_seq_len = cursor.read_i32::<LittleEndian>()?;
-    println!("dim: {}, hidden_dim: {}, n_layers: {}, n_heads: {}, n_kv_heads: {}, vocab_size: {}, max_seq_len: {}", dim, hidden_dim, n_layers, n_heads, n_kv_heads, vocab_size, max_seq_len);
+    let transformer = Transformer::from(path)?;
+    let enc = Tokenizer::default();
 
-    let n_weights = vocab_size * dim;
-    let mut buffer = vec![0; n_weights as usize * 4]; // 4 bytes per float
-
-    // Read the appropriate number of bytes from the file.
-    f.read_exact(&mut buffer)?;
-
-    // Convert bytes to floats
-    let mut cursor = std::io::Cursor::new(buffer);
-    let mut emb_weights = Vec::new();
-    for _ in 0..n_weights {
-        let weight = cursor.read_f32::<LittleEndian>()?;
-        emb_weights.push(weight);
+    let start_ids = enc.encode(start, true, false);
+    let start_ids: Vec<_> = start_ids.into_iter().map(|i| i as usize).collect();
+    let x = Array::from(start_ids).insert_axis(Axis(0));
+    for _ in 0..num_samples {
+        let y = transformer.generate(
+            &mut rng,
+            x.clone(),
+            max_new_tokens,
+            temperature,
+            Some(top_k),
+        );
+        // Ensure there's at least one row
+        if let Some(first_row) = y.outer_iter().next() {
+            let vec_u32: Vec<u32> = first_row.iter().map(|&x| x as u32).collect();
+            println!("{}", enc.decode(&vec_u32));
+            println!("---------------")
+        } else {
+            println!("The 'y' array is empty!");
+        }
     }
-    let _tok_embedding = Embedding::new(emb_weights, vocab_size as usize, dim as usize);
     Ok(())
 }
