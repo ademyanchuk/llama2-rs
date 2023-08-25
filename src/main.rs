cf02c259d52c142babe05ca672a414cd693ff6f1
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2023-08-25 19:59:10 +0200

    Added timers

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod model;
 mod test_data;
 mod tokenizer;
 
+use std::time::Instant;
+
 use anyhow::{Ok, Result};
 
 use ndarray::{Array, Axis};
@@ -15,7 +17,7 @@ use tokenizer::Tokenizer;
 
 fn main() -> Result<()> {
     let path = "stories15M.bin";
-    let start = "One day, Lily met a Shoggoth";
+    let prompt = "One day, Lily met a";
     let num_samples = 2;
     let max_new_tokens = 100; // number of tokens generated in each sample
     let temperature = 1.0; // 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
@@ -24,13 +26,17 @@ fn main() -> Result<()> {
     let seed = [13; 32];
     let mut rng = StdRng::from_seed(seed);
 
+    let start = Instant::now(); // Start timing
     let transformer = Transformer::from(path)?;
+    let duration = start.elapsed(); // Get the elapsed time
+    println!("Time to load model is: {:?} ms\n", duration.as_millis());
     let enc = Tokenizer::default();
 
-    let start_ids = enc.encode(start, true, false);
+    let start_ids = enc.encode(prompt, true, false);
     let start_ids: Vec<_> = start_ids.into_iter().map(|i| i as usize).collect();
     let x = Array::from(start_ids).insert_axis(Axis(0));
     for _ in 0..num_samples {
+        let start = Instant::now();
         let y = transformer.generate(
             &mut rng,
             x.clone(),
@@ -46,6 +52,11 @@ fn main() -> Result<()> {
         } else {
             println!("The 'y' array is empty!");
         }
+        let duration = start.elapsed();
+        println!(
+            "achieved tok/s: {}\n",
+            max_new_tokens as u64 / duration.as_secs()
+        );
     }
     Ok(())
 }
